a9c950e87d4ba00984ef4ab102eca0738bd09bcb
baishali2502 <baishali2502@gmail.com>
2023-11-30 06:11:39 +0530

    Added UC-1:Func. that prints freq. of words

--- HashMap/src/HashMap/HashMap1.java
+++ HashMap/src/HashMap/HashMap1.java
@@ -14,5 +14,29 @@ public class HashMap1 {
 		String str2 = removeWords(str,word);
 		System.out.println("The new string is : "+str2);
 	}
-
+	/*
+	 * @desc:This function prints frequency of words in the string
+	 * 
+	 * @params:The given string 
+	 * 
+	 * @returns:void
+	 */
+	public static void freqOfWords(String str)
+	{
+		HashMap<String,Integer> map = new HashMap<>();
+		String[] arr = str.split(" ");
+		for(int i=0;i<arr.length;i++)
+		{
+			String key = arr[i];
+			if(map.containsKey(key))
+				map.put(key,map.get(key)+1);
+			else
+				map.put(key,1);
+			
+		}
+		for(String key:map.keySet())
+		{
+			System.out.println("Word: "+key+" Freq : "+map.get(key));
+		}
+	}
 }
